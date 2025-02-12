4a9a4a1f31f645c17a0aecb773424a06743390ea
Juan Schandin <jschandin@gmail.com>
2025-02-12 14:39:27 -0300

    isolate type for merge sort

--- sorting-and-searching/merge-sort.c
+++ sorting-and-searching/merge-sort.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MS_T int
 
-void merge(int arr[], int l, int m1, int m2, int r, int tmp[]) {
+void merge(MS_T arr[], int l, int m1, int m2, int r, MS_T tmp[]) {
     while (l <= m1 && m2 <= r) *tmp++ = arr[l] < arr[m2] ? arr[l++] : arr[m2++];
     while (l <= m1) *tmp++ = arr[l++];
     while (m2 <= r) *tmp++ = arr[m2++];
 }
 
-void msort(int arr[], int l, int r, int tmp[]) {
+void msort(MS_T arr[], int l, int r, MS_T tmp[]) {
     if (r <= l) return;
     int m = (l + r) / 2;
     msort(arr, l, m, tmp);
     msort(arr, m + 1, r, tmp);
     merge(arr, l, m, m + 1, r, tmp + l);
-    memcpy(arr + l, tmp + l, (r - l + 1) * sizeof(int));
+    memcpy(arr + l, tmp + l, (r - l + 1) * sizeof(MS_T));
 }
 
-void sort(int arr[], int l, int r) {
-    int *tmp = (int*)malloc((r - l + 1) * sizeof(int));
+void sort(MS_T arr[], int l, int r) {
+    MS_T *tmp = (MS_T*)malloc((r - l + 1) * sizeof(MS_T));
     msort(arr, l, r, tmp);
     free(tmp);
 }
